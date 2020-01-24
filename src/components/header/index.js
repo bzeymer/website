ef43cd9745cad9b73f4937f84f7286b7a9f642ab
Breno Zeymer <bzeymer@gmail.com>
2020-01-24 01:52:04 -0300

    WIP

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import './styles.css';
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
-    <div>
-      Header
+    <div className="header row-wrapper">
+      <div className="content row-content">
+        <Link to="/" className="logo">breno.dev</Link>
+      </div>
     </div>
   );
 }
